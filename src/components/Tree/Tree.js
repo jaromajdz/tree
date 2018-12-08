3044b0f17fc92b4052794dac5283fd5ebd0a3c6f
Jarek Majdzinski <jaromajdz@gmail.com>
2018-12-08 23:18:10 +0000

    Added simple animations.

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -32,11 +32,21 @@ class Tree extends Component {
   //Display ul block or not elem_id is a key from state.tree
 
   showPlus = (elem_id) => {
-      const vis = (this.elems[elem_id].style.display==="block" ? "none" : "block")
-      this.elems[elem_id].style.display = vis;
-      const bt =  {...this.state.buttons}
-      bt[elem_id]=!bt[elem_id]
-      this.setState({buttons: {...bt}})
+        let vis = "block"
+        const bt =  {...this.state.buttons}
+
+      if(this.elems[elem_id].style.display==="block"){
+        vis = "none"
+        bt[elem_id]=false
+        this.setState({buttons: {...bt}})
+        setTimeout(()=>{this.elems[elem_id].style.display = vis;}, 310)
+      }else{
+        bt[elem_id]=true
+        this.setState({buttons: {...bt}})
+        setTimeout(()=>{this.elems[elem_id].style.display = vis;}, 5)
+      }
+
+
       }
 
   //solution for manin ul
@@ -82,7 +92,12 @@ class Tree extends Component {
 
             list.push(cr('li',{key: key+"0"}, [
                                   cr('div',{className: 'lidiv', key: key + "1"}, [ <DButton click={()=>this.showPlus(key)} show={this.state.buttons[key]} key={key+"b" }/> ,name]),
-                                  cr('ul', {style:{display: 'block'}, key: key+"2",ref: el=>this.elems[key]=el} ,this.render_tree(tree[key], level))
+                                  cr('ul',
+                                    { style:{display: 'block'},
+                                      className: (this.state.buttons[key]? 'openul' : 'closeul'),
+                                      key: key+"2",ref: el=>this.elems[key]=el
+                                    } ,
+                                   this.render_tree(tree[key], level))
                                   ]
                                 )
                               )
